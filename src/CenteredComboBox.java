490bec39a816bffcf470d8f00318646fe994f0bb
Fred Morrison <frederick.morrison@outlook.com>
2024-12-19 16:24:16 -0700

    fix warning

--- src/CenteredComboBox.java
+++ src/CenteredComboBox.java
@@ -1,22 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class CenteredComboBox extends JComboBox {
-    public CenteredComboBox(String[] items) {
+public class CenteredComboBox extends JComboBox<String> {
+    private GameController gameController;
+    public CenteredComboBox(String[] items, GameController gameController) {
         super(items);
+        this.gameController = gameController;
     }
 
-    // Customize the renderer to center align the items
-
-/*        @Override
-        public Component getListCellRendererComponent(
-                JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
-            // Call the superclass method for default styling
-            JLabel renderer = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-
-            // Center align the text
-            renderer.setHorizontalAlignment(SwingConstants.CENTER);
-
-            return renderer;
-        }*/
 }
